2828356ef34e4cde62b4adbb4574c6330f1083df
Nnoduka Eruchalu <nceruchalu@gmail.com>
2015-05-09 18:02:50 -0700

    Initial commit

---
+++ Gravvy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Gravvy
+//
+//  Created by Nnoduka Eruchalu on 5/9/15.
+//  Copyright (c) 2015 Nnoduka Eruchalu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
